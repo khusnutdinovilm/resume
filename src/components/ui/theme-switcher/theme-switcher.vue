357a01fc64a258bc7502c0cc64a2c13d07c123db
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-04-20 06:53:12 +0500

    UPD: добавил тень к переключателю темы

--- src/components/ui/theme-switcher/theme-switcher.vue
+++ src/components/ui/theme-switcher/theme-switcher.vue
@@ -30,12 +30,16 @@ const themeSwitcherIcon = computed(() => themesIcon[themePreference.value]);
   align-items: center;
   justify-content: center;
 
-  background-origin: var(--bg-primary);
+  background-origin: var(--bg-quaternary);
   border: 1px solid var(--border-color);
   border-radius: 12px;
   padding: 10px;
 
   cursor: pointer;
+
+  [data-theme="light"] & {
+    box-shadow: 6px 7px 8px 5px rgba(40, 41, 41, 0.6);
+  }
 
   & span {
     font-size: 1.5rem;
