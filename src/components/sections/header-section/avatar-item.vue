701a749d3eb5fa8c6d0aaced03c02dc2750331a8
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-04-22 03:42:47 +0500

    ADD: разработал компонент аватарки

---
+++ src/components/sections/header-section/avatar-item.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="c-avatar">
+    <img :src="avatarUrl" alt="" class="c-avatar__img" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: "avatar-item",
+});
+
+defineProps<{
+  avatarUrl: string;
+}>();
+</script>
+
+<style lang="scss">
+.c-avatar {
+  &__img {
+    border-radius: 50%;
+    width: 11rem;
+
+    @include mx.rs-start-at(md) {
+      width: 14rem;
+    }
+
+    @include mx.rs-start-at(xl) {
+      width: 100%;
+    }
+  }
+}
+</style>
