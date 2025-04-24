67a67fe501e9c3966bbefeb8492575036a0fd761
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-04-24 13:41:17 +0500

    UPD: доработал секцию со списком портфолио

--- src/components/sections/portfolio-section/portfolio-section.vue
+++ src/components/sections/portfolio-section/portfolio-section.vue
@@ -5,15 +5,33 @@
     class="portfolio-section"
   >
     <template #section-content>
-      <!--  -->
+      <portfolio-list />
     </template>
   </app-section>
 </template>
 
 <script setup lang="ts">
+import { onMounted } from "vue";
+
 import AppSection from "shared/app-section";
+
+import usePortfolioStore from "store/portfolio-store";
+
+import PortfolioList from "./portfolio-list.vue";
 
 defineOptions({
   name: "portfolio-section",
 });
+
+const portfolioStore = usePortfolioStore();
+
+onMounted(async () => {
+  try {
+    await portfolioStore.getPortfolioList();
+  } catch (error) {
+    throw error;
+  }
+});
 </script>
+
+<style lang="scss"></style>
