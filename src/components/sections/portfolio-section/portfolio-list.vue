70552d001cbd2db87a9f5bc05bb6348f1d93a420
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-04-24 13:40:31 +0500

    ADD: разработал компонент списка элементов портфолио

---
+++ src/components/sections/portfolio-section/portfolio-list.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="portfolio-list o-list">
+    <div
+      v-for="portfolioItem in portfolioList"
+      :key="portfolioItem.id"
+      class="o-list__item o-content"
+    >
+      <portfolio-list-item :portfolio-item="portfolioItem" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+import usePortfolioStore from "store/portfolio-store";
+
+import PortfolioListItem from "./portfolio-item.vue";
+
+defineOptions({
+  name: "portfolio-list",
+});
+
+const portfolioStore = usePortfolioStore();
+
+const portfolioList = computed(() => portfolioStore.portfolioList);
+</script>
+
+<style lang="scss">
+.portfolio-list {
+  & .portfolio-item {
+    height: 100%;
+  }
+}
+</style>
