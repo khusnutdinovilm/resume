caa8d6ca689cb8dae466fa29885a478a66fe694d
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-04-24 21:07:25 +0500

    UPD: вывел список образований на страницу

--- src/components/sections/education-section/education-section.vue
+++ src/components/sections/education-section/education-section.vue
@@ -5,15 +5,56 @@
     class="education-section"
   >
     <template #section-content>
-      <!--  -->
+      <div class="o-content">
+        <timeline-list :timeline-items="educationList" with-end />
+      </div>
     </template>
   </app-section>
 </template>
 
 <script setup lang="ts">
+import { computed, onMounted } from "vue";
+
 import AppSection from "shared/app-section";
+import TimelineList from "shared/timeline-list";
+
+import useEducationStore from "store/education-store";
 
 defineOptions({
   name: "education-section",
 });
+
+const educationStore = useEducationStore();
+
+const educationList = computed(() => educationStore.educationList);
+
+onMounted(async () => {
+  try {
+    await educationStore.getEducationList();
+  } catch (error) {
+    throw error;
+  }
+});
 </script>
+
+<style lang="scss">
+.education-section {
+  & .o-section__content {
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+
+  & .timeline {
+    padding-top: 1.75rem;
+    margin-left: calc(1rem - 2px);
+
+    @include mx.rs-start-at(md) {
+      padding-top: 3.15rem;
+    }
+
+    @include mx.rs-start-at(xl) {
+      padding-top: 4.375rem;
+    }
+  }
+}
+</style>
