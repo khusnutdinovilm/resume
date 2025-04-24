2c1324c6c3dc5ed0b224bdfafac6f75530e46800
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-04-24 21:06:31 +0500

    UPD: вывел опыт работы на странице

--- src/components/sections/experience-section/experience-section.vue
+++ src/components/sections/experience-section/experience-section.vue
@@ -5,15 +5,43 @@
     class="experience-section"
   >
     <template #section-content>
-      <!--  -->
+      <timeline-list :timeline-items="expirienceList" />
     </template>
   </app-section>
 </template>
 
 <script setup lang="ts">
+import { computed, onMounted } from "vue";
+
 import AppSection from "shared/app-section";
+import TimelineList from "shared/timeline-list";
+
+import useExpirienceStore from "store/expirience-store";
 
 defineOptions({
   name: "experience-section",
 });
+
+const expirienceStore = useExpirienceStore();
+const expirienceList = computed(() => expirienceStore.expirienceList);
+
+onMounted(async () => {
+  try {
+    await expirienceStore.getExpirienceList();
+  } catch (error) {
+    throw error;
+  }
+});
 </script>
+
+<style lang="scss">
+.experience-section {
+  & .o-section__content {
+    padding-bottom: 0;
+  }
+
+  & .timeline {
+    margin-left: calc(1rem - 2px);
+  }
+}
+</style>
